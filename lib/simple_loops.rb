2180c0a5c46567035c69d72ee68f5d34470d91cd
Don Mallory <dmm333@gmail.com>
2020-04-15 00:33:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,15 @@
-# Write your methods here
+def loop_message_five_times(string)
+  count = 0
+  while count < 5
+    puts string
+    count += 1
+  end
+end
+
+def loop_message_n_times(string, n)
+  count = 0
+  while count < n
+    puts string
+    count += 1
+  end
+end
